b4d6679b28e0f53795e54fcab4e1593fe19b0023
krk224 <hihas93@naver.com>
2021-02-16 12:55:56 +0900

    weekInfo finished

--- front/src/components/content_attendance.js
+++ front/src/components/content_attendance.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react"
-import WeekComponent from "./WeekComponent"
+// import WeekComponent from "./WeekComponent"
 
 class ContentAttend extends Component {
   //render 필수
@@ -7,41 +7,75 @@ class ContentAttend extends Component {
     super(props);
     this.state = {
         userId: 0,
-        period : 2,
-        weeks: 11
-
+        // period : 2,
+        totalweeks: 11,
+        attendList: [1,1,0,1,0]
     }
   }
-
-  setUserInfo = (ID, prd, weekCnt) =>{
+  setUserInfo = (ID, weekCnt, attList) =>{
     this.setState({
       userID: ID,
-      period: prd,
-      weeks: weekCnt
+      // period: prd,
+      weeks: weekCnt,
+      attendList: attList
     })
   }
 
   render() {
-    console.log("Content attendance render");
 
+    console.log("Content attendance render");
     const borderStyle = {
       border: "1px solid black"
     }
+    const weekList = [];
+
+
+    for(let i =0; i <this.state.totalweeks; ++i){
+        weekList.push({weekIdx: i, attendance: this.state.attendList[i]});
+    }
+    console.log(weekList);
+
+    function convertAttend(attend) {
+      if(attend == 0){
+        return "결석 😢";
+      }
+      else {
+        return attend && "출석 😊";
+      }
+
+    }
+
+    const weekRender = weekList.map((week, i) => {
+      return (
+        <div key = {i} className = "AttendTable">
+
+        <div className = "WeekIdx">{week.weekIdx+1} 주차</div>
+        <div className = {this.state.attendList[i] === 1? "present" :(this.state.attendList[i] === 0 ? "absent": "Undefined")}>
+
+          {convertAttend(week.attendance)}
+
+        </div>
+
+        </div>
+      );
+    })
+
+
     return (
       <div className = "wrapperAttend">
-        <div className = "NavAttend" style = {borderStyle}>
+        <div className = "NavAttend">
           <ol>
             <li> 출석부 </li>
             <li> 주간목표 </li>
           </ol>
         </div>
 
-        <div className = "MainAttend" style = {borderStyle}>
-          <div className = "AttendTable" style = {borderStyle}>
-
-          </div>
+        <div className = "MainAttend">
+            {weekRender}
         </div>
       </div>
+
+
 
     )
   }
