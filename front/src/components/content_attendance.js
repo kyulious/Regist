b30a8246b08e55719eec6b34738e3e2d69e73fcd
krk224 <hihas93@naver.com>
2021-02-16 16:17:51 +0900

    master-merge

---
+++ front/src/components/content_attendance.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+
+class ContentAttend extends Component {
+  //render 필수
+  render() {
+    console.log("TIL render")
+    return (
+      <div class="wrapper">
+        <h1>ContentAttend</h1>
+      </div>
+    )
+  }
+}
+
+export default ContentAttend
